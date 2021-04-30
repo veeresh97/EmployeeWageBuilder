a3291ff5b080814a9285a6b53bd547ea7b7425ab
veeresh97 <67183089+veeresh97@users.noreply.github.com>
2021-04-30 22:52:56 +0530

    Added: wage calcultion for max number of days

--- employeeWageBuilder.java
+++ employeeWageBuilder.java
@@ -1,30 +1,32 @@
 
 public class employeeWageBuilder {
-	public static final int Is_FullTime = 1;
-	public static final int Is_PartTime = 2;
-	public static final int Wage_per_hour = 20;
-	public static final int Total_work_days = 20;
-	
-		public static void main(String [] args) {
-		int emphrs = 0;
-		int EmpWage = 0;
-		int TotalWage = 0;
-		for (int day = 0; day<Total_work_days; day++) {
-			int empCheck =(int) Math.floor(Math.random() * 10) % 3;
-			switch (empCheck) {
-			case Is_FullTime:
-				emphrs = 8;
-				break;
-			case Is_PartTime:
-				emphrs = 4;
-				break;
-			default:
-			   emphrs = 0;
-		}
-		EmpWage = Wage_per_hour * emphrs;
-		TotalWage += EmpWage;
-		System.out.println("Employee wage is: " + EmpWage);
-	}
-	System.out.println("Total Employee wage is :" + TotalWage);
-  }
+	public static final int PRESENT_FULL_TIME = 2;
+    public static final int PRESENT_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 8;
+    public static final int IS_PART_TIME = 4;
+    public static final int PER_HOUR_WAGE = 20;
+
+    public static void main(String[] args) {
+        int work_hours = 0, work_days = 0;
+        double salary = 0;
+        while (work_hours < 100 && work_days < 20) {
+            work_days++;
+            double empcheck = Math.floor(Math.random() * 10) % 3;
+            int check = (int) empcheck;
+            switch (check) {
+                case PRESENT_FULL_TIME:
+                    work_hours += IS_FULL_TIME;
+                    break;
+                case PRESENT_PART_TIME:
+                    work_hours += IS_PART_TIME;
+                    break;
+                default:
+                    work_hours += 0;
+            }
+            System.out.println("Day" +work_days + " Total working hrs "+work_hours);
+        }
+        salary = work_hours * PER_HOUR_WAGE;
+        System.out.println("total working hours " + work_hours + " total working days " + work_days);
+        System.out.println("salary : " + salary);
+    }
 }
