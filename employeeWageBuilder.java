be6bc48eb7635137f6652dea2356f600bfe48d04
veeresh97 <67183089+veeresh97@users.noreply.github.com>
2021-04-30 22:49:52 +0530

    Added: wage calculation for month

--- employeeWageBuilder.java
+++ employeeWageBuilder.java
@@ -3,24 +3,28 @@ public class employeeWageBuilder {
 	public static final int Is_FullTime = 1;
 	public static final int Is_PartTime = 2;
 	public static final int Wage_per_hour = 20;
+	public static final int Total_work_days = 20;
 	
 		public static void main(String [] args) {
 		int emphrs = 0;
 		int EmpWage = 0;
-		
-		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
-		switch (empCheck) {
-		case Is_FullTime:
-			emphrs = 8;
-			break;
-		case Is_PartTime:
-			emphrs = 4;
-			break;
-		default:
-		   emphrs = 0;
+		int TotalWage = 0;
+		for (int day = 0; day<Total_work_days; day++) {
+			int empCheck =(int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			case Is_FullTime:
+				emphrs = 8;
+				break;
+			case Is_PartTime:
+				emphrs = 4;
+				break;
+			default:
+			   emphrs = 0;
 		}
-		
 		EmpWage = Wage_per_hour * emphrs;
+		TotalWage += EmpWage;
 		System.out.println("Employee wage is: " + EmpWage);
 	}
+	System.out.println("Total Employee wage is :" + TotalWage);
+  }
 }
