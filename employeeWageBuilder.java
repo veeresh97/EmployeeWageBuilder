505956eb96d40523b9f9a44ccbbe542b6a7dc35f
veeresh97 <67183089+veeresh97@users.noreply.github.com>
2021-04-30 22:35:31 +0530

    Added: Attendence check

--- employeeWageBuilder.java
+++ employeeWageBuilder.java
@@ -1,7 +1,13 @@
 
 public class employeeWageBuilder {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation");
+		int Is_FullTime = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == Is_FullTime)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is Absent");
 
-   }
+	}
+
 }
