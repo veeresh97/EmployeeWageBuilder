e3cc23503f94a445ffe1c3a0d381db5e94e8ffc5
veeresh97 <67183089+veeresh97@users.noreply.github.com>
2021-04-30 22:46:03 +0530

    Added: Siwtch case method is used

--- employeeWageBuilder.java
+++ employeeWageBuilder.java
@@ -1,25 +1,26 @@
 
 public class employeeWageBuilder {
-	public static void main(String[] args) {
-		int Is_FullTime = 1;
-		int Is_PartTime = 2;
-		int Wage_per_hour = 20;
-		
+	public static final int Is_FullTime = 1;
+	public static final int Is_PartTime = 2;
+	public static final int Wage_per_hour = 20;
+	
+		public static void main(String [] args) {
 		int emphrs = 0;
 		int EmpWage = 0;
 		
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if (empCheck == Is_FullTime)
+		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
+		switch (empCheck) {
+		case Is_FullTime:
 			emphrs = 8;
-		else if (empCheck == Is_PartTime)
+			break;
+		case Is_PartTime:
 			emphrs = 4;
-		else      
-		    emphrs = 0;
+			break;
+		default:
+		   emphrs = 0;
+		}
 		
 		EmpWage = Wage_per_hour * emphrs;
 		System.out.println("Employee wage is: " + EmpWage);
-
 	}
-
-
 }
